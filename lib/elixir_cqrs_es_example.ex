e445c28c34e26191b3da9a41512a8dceb80d386b
JoeBew42 <joebew42@gmail.com>
2017-12-16 12:11:10 +0100

    initial commit

---
+++ lib/elixir_cqrs_es_example.ex
@@ -0,0 +1,18 @@
+defmodule ElixirCqrsEsExample do
+  @moduledoc """
+  Documentation for ElixirCqrsEsExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirCqrsEsExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_cqrs_es_example_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirCqrsEsExampleTest do
+  use ExUnit.Case
+  doctest ElixirCqrsEsExample
+
+  test "greets the world" do
+    assert ElixirCqrsEsExample.hello() == :world
+  end
+end
