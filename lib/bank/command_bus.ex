86cc11b8e36ea75abf3a2034fedc6effbbe75705
JoeBew42 <joebew42@gmail.com>
2018-04-11 09:45:12 +0200

    rename event in command

--- lib/bank/command_bus.ex
+++ lib/bank/command_bus.ex
@@ -9,16 +9,16 @@ defmodule Bank.CommandBus do
     GenServer.call(:command_bus, subscriber_pid)
   end
 
-  def publish(event) do
-    GenServer.cast(:command_bus, {:publish, event})
+  def publish(command) do
+    GenServer.cast(:command_bus, {:publish, command})
   end
 
   def handle_call(subscriber_pid, _pid, subscribers) do
     {:reply, :ok, [subscriber_pid | subscribers]}
   end
 
-  def handle_cast({:publish, event}, subscribers) do
-    Enum.each(subscribers, fn(subscriber) -> send(subscriber, event) end )
+  def handle_cast({:publish, command}, subscribers) do
+    Enum.each(subscribers, fn(subscriber) -> send(subscriber, command) end )
     {:noreply, subscribers}
   end
 end
