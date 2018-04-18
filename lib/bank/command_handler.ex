320540bc6a5b347779bdc698a3faff7f73ed39ca
JoeBew42 <joebew42@gmail.com>
2018-04-18 08:30:55 +0200

    wip: trivial implementation for deposit money

--- lib/bank/command_handler.ex
+++ lib/bank/command_handler.ex
@@ -1,8 +1,8 @@
 defmodule Bank.CommandHandler do
   use GenServer
 
-  alias Bank.Commands.CreateAccount
-  alias Bank.EventStore
+  alias Bank.Commands.{CreateAccount, DepositMoney}
+  alias Bank.{EventStore,  EventStream}
   alias Bank.Account
 
   def start_link(_opts) do
@@ -18,6 +18,18 @@ defmodule Bank.CommandHandler do
     Account.create(pid, command.id)
 
     {:ok, _version} = EventStore.append_to_stream(command.id, -1, Account.changes(pid))
+
+    {:reply, :ok, nil}
+  end
+
+  def handle_call(command = %DepositMoney{}, _pid, nil) do
+    {:ok, event_stream} = EventStore.load_event_stream(command.id)
+
+    {:ok, pid} = Account.new
+    Account.load_from_event_stream(pid, event_stream)
+    Account.deposit(pid, command.amount)
+
+    {:ok, _version} = EventStore.append_to_stream(command.id, event_stream.version, Account.changes(pid))
 
     {:reply, :ok, nil}
   end
