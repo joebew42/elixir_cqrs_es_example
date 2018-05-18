40329ba2c4031bc2e592e8a6735947ae679a9aae
JoeBew42 <joebew42@gmail.com>
2018-05-18 14:54:16 +0200

    remove the TestableCommandHandler

--- test/bank/command_bus_test.exs
+++ test/bank/command_bus_test.exs
@@ -2,10 +2,8 @@ defmodule Bank.CommandBusTest do
   use ExUnit.Case, async: true
 
   setup_all do
-    {:ok, command_handler_pid} = start_supervised TestableCommandHandler
     {:ok, _pid} = start_supervised Bank.CommandBus
-
-    %{command_handler_pid: command_handler_pid}
+    :ok
   end
 
   test "not subscribed handler do not receive events" do
@@ -14,11 +12,11 @@ defmodule Bank.CommandBusTest do
     refute_receive {:create_account, "joe"}
   end
 
-  test "subscribed handler receive events", %{command_handler_pid: command_handler_pid} do
-    Bank.CommandBus.subscribe(command_handler_pid)
+  test "subscribed handler receive events" do
+    Bank.CommandBus.subscribe(self())
 
     Bank.CommandBus.publish({:create_account, "joe"})
 
-    assert TestableCommandHandler.received? {:create_account, "joe"}
+    assert_receive {_, _, {:create_account, "joe"}}
   end
 end
