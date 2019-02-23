004fa8abdba53287b326bc7420d01ad9e184fa0a
JoeBew42 <joebew42@gmail.com>
2019-02-23 19:20:16 +0100

    provide a naive implementation of the inmemory event store

--- lib/bank/in_memory_event_store.ex
+++ lib/bank/in_memory_event_store.ex
@@ -8,8 +8,8 @@ defmodule Bank.InMemoryEventStore do
   end
 
   @impl Bank.EventStore
-  def load_event_stream(_aggregate_id) do
-    {:error, :not_found}
+  def load_event_stream(aggregate_id) do
+    GenServer.call(__MODULE__, {:load_event_stream, aggregate_id})
   end
 
   @impl Bank.EventStore
@@ -23,11 +23,24 @@ defmodule Bank.InMemoryEventStore do
   end
 
   @impl true
-  def handle_call({:append_to_stream, aggregate_id, version, changes}, _from, state) do
-    state = %{
-      "aggregate_id" => []
-    }
+  def handle_call({:load_event_stream, aggregate_id}, _from, state) do
+    reply = case Map.get(state, aggregate_id) do
+      nil ->
+        {:error, :not_found}
+      events ->
+        {:ok, 0, events}
+    end
 
-    {:reply, :ok, state}
+    {:reply, reply, state}
+  end
+
+  @impl true
+  def handle_call({:append_to_stream, aggregate_id, _version, changes}, _from, state) do
+    events = Map.get(state, aggregate_id, [])
+    updated_events = changes ++ events
+
+    new_state = Map.put(state, aggregate_id, updated_events)
+
+    {:reply, :ok, new_state}
   end
 end
