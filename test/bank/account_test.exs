c131ea14ae2a0aad06c67164bcf713aab63cd6f2
JoeBew42 <joebew42@gmail.com>
2019-04-07 09:53:50 +0200

    use describe for each function of account

--- test/bank/account_test.exs
+++ test/bank/account_test.exs
@@ -4,36 +4,41 @@ defmodule Bank.AccountTest do
   alias Bank.Events.{AccountCreated, MoneyDeposited, MoneyWithdrawalDeclined, MoneyWithdrawn}
   alias Bank.Account
 
-  describe "with a new account" do
-    setup do
-      account = Account.new(%Account{}, "Joe")
+  describe "#new" do
+    test "produces an AccountCreated change" do
+      account =
+        %Account{}
+        |> Account.new("Joe")
 
-      %{account: account}
-    end
-
-    test "#new produces an AccountCreated change", %{account: account} do
       assert account |> contain_change?(%AccountCreated{id: "Joe"})
     end
+  end
 
-    test "#deposit produces a MoneyDeposited change", %{account: account} do
+  describe "#deposit" do
+    test "produces a MoneyDeposited change" do
       account =
-        account
+        %Account{}
+        |> Account.new("Joe")
         |> Account.deposit(100)
 
       assert account |> contain_change?(%MoneyDeposited{id: "Joe", amount: 100})
     end
+  end
 
-    test "#withdraw produces a MoneyWithdrawalDeclined if there is no sufficient funds", %{account: account} do
+  describe "#withdraw" do
+    test "produces a MoneyWithdrawalDeclined if there is no sufficient funds" do
       account =
-        account
+        %Account{}
+        |> Account.new("Joe")
         |> Account.withdraw(100)
 
       assert account |> contain_change?(%MoneyWithdrawalDeclined{id: "Joe", amount: 100})
     end
 
-    test "#withdraw produces a MoneyWithdrawn", %{account: account} do
+    test "produces a MoneyWithdrawn" do
       account =
-        account
+        %Account{}
+        |> Account.new("Joe")
         |> Account.deposit(100)
         |> Account.withdraw(100)
 
@@ -41,14 +46,16 @@ defmodule Bank.AccountTest do
     end
   end
 
-  test "#load_from_events load the state from events" do
-    events = [
-      %MoneyWithdrawn{id: "Joe", amount: 50},
-      %MoneyDeposited{id: "Joe", amount: 100},
-      %AccountCreated{id: "Joe"},
-    ]
+  describe "#load_from_events" do
+    test "load the state from events" do
+      events = [
+        %MoneyWithdrawn{id: "Joe", amount: 50},
+        %MoneyDeposited{id: "Joe", amount: 100},
+        %AccountCreated{id: "Joe"},
+      ]
 
-    assert Account.load_from_events(events) == %Account{id: "Joe", amount: 50, changes: []}
+      assert Account.load_from_events(events) == %Account{id: "Joe", amount: 50, changes: []}
+    end
   end
 
   defp contain_change?(account, event) do
