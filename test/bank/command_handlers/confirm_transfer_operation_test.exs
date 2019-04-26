61bfc39739f4229c5b15b07181230b4f64efe603
JoeBew42 <joebew42@gmail.com>
2019-04-26 20:43:49 +0200

    todo: confirm transfer operation command

---
+++ test/bank/command_handlers/confirm_transfer_operation_test.exs
@@ -0,0 +1,15 @@
+defmodule Bank.CommandHandlers.ConfirmTransferOperationTest do
+  use ExUnit.Case, async: true
+
+  import Mox
+
+  alias Bank.{Events, Commands}
+
+  alias Bank.EventStoreMock, as: EventStore
+
+  alias Bank.CommandHandlers.ConfirmTransferOperation
+
+  test "xxx" do
+    assert false
+  end
+end
